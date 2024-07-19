99e371737e08796ddccac046089d2ef7fbadc1d9
Minh Nguyen <caominh11122000@gmail.com>
2024-07-19 15:15:49 -0300

    Added joystick teleoperation control
    Added a joystick teleop launch files with two nodes
    - joystick node for reading the PS4 joystick command and publish it on a ROS2 topic
    - joy_teleop node reads the PS4 command published by the node above, convert it to a TwistStamped type message for the bumperbot_control/cmd_vel topic. The existing simple_controller that was created in an earlier version converts the desired robot speed to the wheel speed and control the robot in Gazebo simulation
    
    Created two yaml configuration files:
    - One file is for the joystick node to setup some settings of the PS4 controller
    - One file is for the joy_teleop node. This file declares the type of message, the topic that the node should publish to, and the axis mapping from the PS4 joysticks to the desired linear velocity and angular velocity

---
+++ src/bumperbot_controller/launch/joystick_teleop.launch.py
@@ -0,0 +1,36 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+
+# THE CONTROL PIPELINE:
+# 1. User use the joysticks on a PS4 controller, that message is read and published on a ROS2 topic by the node "joystick"
+# 2. The published message is read by the "joy_teleop" node, converted to a TwistStamped message, and then published to the "bumperbot_controller/cmd_vel" topic
+# 3. The "simple_controller" node subscribes to the "bumperbot_controller/cmd_vel" topic, converts the robot velocity to the required wheel speed
+# 4. The wheel speed is published to the topic "simple_velocity_controller/commands", which controls and actuate the wheel
+
+def generate_launch_description():
+
+    joy_node = Node(
+    # This node reads the joystick command and publishes it within a ROS2 topic
+        package = "joy",
+        executable = "joy_node",
+        name = "joystick",
+        parameters = [os.path.join(get_package_share_directory("bumperbot_controller"),"config","joy_config.yaml")]
+    )
+
+
+    joy_teleop = Node(
+    # This node converts the joystick command of a PS4 controller (published by the node above) to a TwistStamped message
+    # This message type is expected by the bumperbot_controller/cmd_vel node
+        package = "joy_teleop",
+        executable = "joy_teleop",
+        parameters = [os.path.join(get_package_share_directory("bumperbot_controller"),"config","joy_teleop.yaml")]
+    )
+
+    return LaunchDescription([
+        joy_node,
+        joy_teleop
+    ])
